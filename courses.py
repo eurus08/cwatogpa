e01e13d38c28a3d8071d0d90c054c0d521243871
eurus08 <asumadu08@gmail.com>
2025-08-31 18:50:42 +0000

    Add another sem

--- courses.py
+++ courses.py
@@ -1,8 +1,17 @@
 courses = [
     #  sem 1
-    (75, 3),  # scored 75 in a 3-credit course
-    (62, 2),  # scored 62 in a 2-credit course
-    (55, 3),  # scored 55 in a 3-credit course
-    (48, 2),  # scored 48 in a 2-credit course
-    (38, 1)   # scored 38 in a 1-credit course
+    (52, 2),  # scored 75 in a 3-credit course
+    (57, 2),  # scored 62 in a 2-credit course
+    (79, 3),  # scored 55 in a 3-credit course
+    (68, 3),  # scored 48 in a 2-credit course
+    (60, 3),   # scored 38 in a 1-credit course
+    (76, 3),
+    (73 ,2),
+    # Sem 2 
+    (85, 2),
+    (50, 3),
+    (80, 3),
+    (90, 3),
+    (67, 3),
+    (73, 3)
 ] 
